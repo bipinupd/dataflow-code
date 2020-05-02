00dfb9e01c51489ff4448f87a06a21160e5d7a65
bipinupd <bipinupd@gmail.com>
2020-05-01 23:35:53 -0400

    Test

--- src/test/java/org/apache/beam/examples/WordCountTest.java
+++ src/test/java/org/apache/beam/examples/WordCountTest.java
@@ -3,42 +3,21 @@ package org.apache.beam.examples;
 import java.util.Arrays;
 import java.util.List;
 import org.apache.beam.examples.WordCount.CountWords;
-import org.apache.beam.examples.WordCount.ExtractWordsFn;
 import org.apache.beam.examples.WordCount.FormatAsTextFn;
 import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.testing.ValidatesRunner;
 import org.apache.beam.sdk.transforms.Create;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.DoFnTester;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.values.PCollection;
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+
 
 /** Tests of WordCount. */
-@RunWith(JUnit4.class)
+
 public class WordCountTest {
-
- /** Example test that tests a specific {@link DoFn}. */
- @Test
- public void testExtractWordsFn() throws Exception {
-  DoFnTester < String, String > extractWordsFn = DoFnTester.of(new ExtractWordsFn());
-
-  Assert.assertThat(
-   extractWordsFn.processBundle(" some  input  words "),
-   CoreMatchers.hasItems("some", "input", "words"));
-  Assert.assertThat(extractWordsFn.processBundle(" "), CoreMatchers.hasItems());
-  Assert.assertThat(
-   extractWordsFn.processBundle(" some ", " input", " words"),
-   CoreMatchers.hasItems("some", "input", "words"));
- }
 
  static final String[] WORDS_ARRAY =
   new String[] {
@@ -59,12 +38,12 @@ public class WordCountTest {
    "two: 2",
    "one: 1"
  };
+@Rule
 
- @Rule public TestPipeline p = TestPipeline.create();
+ public TestPipeline p = TestPipeline.create();
 
  /** Example test that tests a PTransform by using an in-memory input and inspecting the output. */
  @Test
- @Category(ValidatesRunner.class)
  public void testCountWords() throws Exception {
   PCollection < String > input = p.apply(Create.of(WORDS).withCoder(StringUtf8Coder.of()));
 
